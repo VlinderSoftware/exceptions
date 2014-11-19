1df0038d191433af32b0f7e8a96a5952e3698200
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2014-11-18 23:23:52 -0500

    reflect header rename for Windows

--- contract.win32.cpp
+++ contract.win32.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "contract.h"
+#include "contract.hpp"
 #include <sstream>
 #include <windows.h>
 using namespace std;
