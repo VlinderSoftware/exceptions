674f9689ddb37629649cb35be5d518955c512347
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2014-11-18 21:26:25 -0500

    Copy contract.{other,win32}.cpp over from Vlinder branch, and remove Contract.cpp (which is replaced)

--- contract.other.cpp
+++ contract.other.cpp
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "Contract.h"
+#include "contract.h"
 #include <sstream>
-
 using namespace std;
 
 namespace Vlinder { namespace Contract {
