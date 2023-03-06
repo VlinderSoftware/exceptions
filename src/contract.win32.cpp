c1fc1872ca555d24bdbfccb031e7aa5554e0b0f3
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2023-03-05 23:02:03 -0500

    Fix the Windows port

--- src/contract.win32.cpp
+++ src/contract.win32.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 namespace Vlinder { namespace Contract {
-	EXCEPTIONS_API void reportFailure(const char *preamble, const char *expression, const char *file, int line, const char *function)
+	exceptions_EXPORT void reportFailure(const char *preamble, const char *expression, const char *file, int line, const char *function)
 	{
 		stringstream ss;
 		ss
